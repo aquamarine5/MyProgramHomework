e07237c76bbc47b9e3cc9851b0719deb120335ed
aquamarine5 <3168287806@QQ.com>
2024-11-19 21:10:23 +0800

    rename +hw5

---
+++ 20241119-exp3.py
@@ -0,0 +1,30 @@
+def topic1():
+    print(f"{[c for c in input() if int(c)%2==0]}")
+    
+def topic2():
+    def fun(n:int)->bool:
+        return n%3==0 and n%5!=0
+    print(f"{[n for n in range(100,int(input())) if fun(n)]}")
+    
+def topic3():
+    def isprime(n:int)->bool:
+        if n<2: return False
+        for i in range(2,n):
+            if n%i==0:
+                return False
+        return True
+    def sum(n:int)->int:
+        return sum([i for i in range(2,n) if isprime(i)])
+    print(f"{sum(int(input()))}")
+
+def topic4():
+    def ave(l:list)->float:
+        return sum(l)/len(l)
+    print(f"{ave([float(i) for i in input().split(',')])}")
+
+def topic5():
+    def k(n:int)->int:
+        if n==0: return 1
+        if n==1: return 2
+        return k(n-1)**2+k(n-2)**2
+    print(f"{k(int(input()))}") 
